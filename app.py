ed5a457270c8ea1e919364cfa6765c17d269712c
Andrei Kardovich <akardovich@gmail.com>
2022-07-18 19:51:28 +0300

    add button detail

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db' # подключение базы данных
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
